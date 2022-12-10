66e379d90be29bf53e0d48b7497cb67add0192ec
AryanMore <aryanmore498@gmail.com>
2022-12-10 15:37:58 +0530

    boilerplate server setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost";
+
+// start
+
+app.listen(3000, function() {
+    console.log("server is running on port number 3000");
+});
