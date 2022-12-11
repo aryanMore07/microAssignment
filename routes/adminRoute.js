88a955e79c8550250fcb3fad90dcb4ceb6990618
AryanMore <aryanmore498@gmail.com>
2022-12-11 14:07:35 +0530

    insert image with details api

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@ const {Router} = require("express");
 const Gallery = require("../models/galleryCategory");
 const route = Router();
 const Category = require("../models/galleryCategory");
-// const image = require("../models/imagegallery");
+const Image = require("../models/imagegallery");
 
 let time = new Date().toLocaleString();
 console.log(time);
@@ -21,6 +21,25 @@ route.post("/addcategory", (req, res) => {
     newGallery.save((err) => {
         if(!err) {
             res.send("Succesfully added a new category");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+route.post("/addimage", (req, res) => {
+    const newImage = new Image({
+        name: req.body.name,
+        createdAt: time,
+        updatedAt:time,
+        category: [],
+        likes: req.body.like,
+        imageLink: req.body.imageLink,
+    })
+
+    newImage.save((err) => {
+        if(!err) {
+            res.send("Succesfully added the Image");
         } else {
             res.send(err);
         }
